acc625a75a335e5341be017c97024cca4e0f7987
Swty2021 <95666828+Swty2021@users.noreply.github.com>
2021-12-08 18:35:02 +0530

    Add files via upload

---
+++ variable_examples.py
@@ -0,0 +1,53 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+var10 = "python"
+10var = "python"
+SyntaxError: invalid decimal literal
+a = 10
+a = "Python"
+print a
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+a = 10
+a = "Python"
+print (a)
+Python
+a = 10
+A = "Python"
+print (a)
+10
+print (A)
+Python
+print (a,A)
+10 Python
+print (a+A)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    print (a+A)
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+a = b = c = 10
+print (a)
+10
+print (b)
+10
+print (c)
+10
+user_input = 10
+user_Input = 20
+print (user_input)
+10
+print (userInput)
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    print (userInput)
+NameError: name 'userInput' is not defined. Did you mean: 'user_Input'?
+print (user_Input)
+20
+abcdf98765 = "python"
+print (abcdf98765)
+python
+_A = 5
+print (_A)
+5
+and = 5
+SyntaxError: invalid syntax
+#using keyword as variable name
