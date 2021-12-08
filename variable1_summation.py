cc9d3bcb9359a3d54d898de9eec34f1e032a0a63
Swty2021 <95666828+Swty2021@users.noreply.github.com>
2021-12-08 18:17:59 +0530

    Add files via upload

---
+++ variable1_summation.py
@@ -0,0 +1,10 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a = 10
+b = 20
+print(a+b)
+SyntaxError: multiple statements found while compiling a single statement
+a = 10
+b = 20
+print (a+b)
+30
