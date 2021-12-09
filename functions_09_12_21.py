36e56e3ed035d4bb32b974a5ef86bc776cd2d54e
Swty2021 <95666828+Swty2021@users.noreply.github.com>
2021-12-09 20:09:25 +0530

    Add files via upload

---
+++ functions_09_12_21.py
@@ -0,0 +1,36 @@
+print ('Hello! World')
+print ('Hello! World',end='.')
+print ('Welcome to Python')
+print ('Hello! World\n Welcome to Python')
+b = 'Learn'
+print ("Hello! World",b,"Python")
+
+## finding the types
+a = ('apple','banana','mango','orange')
+b1 = ("Learn Python")
+c = 22
+type1 = type(a)
+type2 = type(b1)
+type3 = type(c)
+print (type1)
+print (type2)
+print (type3)
+print (type1,type2,type3)
+
+##finding the length
+lang = ['C','Python','PHP','PERL']
+length = len(lang)
+print (length)
+
+length_b = len(b)
+print (length_b)
+print (len(b1))
+
+##submitting input string
+name = input("Enter your name:")
+print ("Welcome, to learn Python" , name)
+
+print ("Enter your mobile no:")
+mobile_no = input()
+print ("Your mobile no is :" + mobile_no)
+
