fa2b91d49dfbc3826316dbbcfc4d928bf30dcbc8
Swty2021 <95666828+Swty2021@users.noreply.github.com>
2021-12-10 19:19:52 +0530

    Add files via upload

---
+++ area_perimeter_circle_10-12-21.py
@@ -0,0 +1,21 @@
+# Area of a circle
+rad = 7
+pi = 22/7
+area = pi*rad*rad
+print ("Area of the circle is:", area)
+
+# Perimeter of a circle
+rad = 10.2
+pi = 3.14
+perimeter = 2*pi*rad
+print ("Perimenter of the circle is:", perimeter)
+
+#Area perimeter using math
+import math
+rad = 7
+rad1 = 10.2
+area = math.pi*rad*rad
+perimeter = 2*math.pi*rad1
+print ('Area of the circle is:',area)
+print ('Perimeter of the circle is:',perimeter)
+            
